ad59618fd4b675de881c5f85476aa2cdfe61993d
RazeManer <mr.yger001@gmail.com>
2025-06-16 14:05:48 +0300

    Update db.py
    comm

--- db.py
+++ db.py
@@ -1,1 +1,17 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base, Mapped, mapped_column
+
+DB_URL = 'sqlite:///db/database.db'
+engine = create_engine(DB_URL, echo=True)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+
+
+def create_db_and_tables() -> None:
+	Base.metadata.create_all(engine)
