35f73f9cdce12a0841b36586ba4a0e2cb8489db0
RazeManer <mr.yger001@gmail.com>
2025-06-21 16:50:10 +0300

    User_subject_joinedTS_added

--- db.py
+++ db.py
@@ -1,17 +1,85 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
-from sqlalchemy.orm import relationship, sessionmaker, declarative_base, Mapped, mapped_column
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table, DateTime, Boolean
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base, Mapped, mapped_column, scoped_session
+import sqlalchemy
+from datetime import datetime, timezone
+from typing import List
 
-DB_URL = 'sqlite:///db/database.db'
+DB_URL = 'sqlite:///database.db'
 engine = create_engine(DB_URL, echo=True)
+Session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
+
+user_subject_association = Table(
+    "joinedTS",
+    Base.metadata,
+    Column("u_id", Integer, ForeignKey("users.id"), primary_key=True),
+    Column("s_id", Integer, ForeignKey("subjects.id"), primary_key=True) 
+)
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    username: Mapped[str] = mapped_column(String, nullable=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    clas: Mapped[int] = mapped_column(Integer, nullable=False)
+    registration_time: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(tz=timezone.utc), nullable=False)
+    warn: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_admin: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    is_teacher: Mapped[int] = mapped_column(Integer, nullable=False)#0 - не препод, 1 - не подтверждённый препод, 2 - подтверждённый препод
+    busy: Mapped[int] = mapped_column(Integer, nullable=False)#0 - не занят, 1 - занят как ученик, 2 - занят как препод, 12 - занят и как препод и как ученик
 
+
+    items: Mapped[List["Subject"]] = relationship(
+        secondary=user_subject_association,
+        back_populates="users"
+    )
+
+
+class Subject(Base):
+     __tablename__ = 'subjects'
+     
+     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True, nullable=False)
+     subject: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+
+     users: Mapped[List["User"]] = relationship(
+        secondary=user_subject_association,
+        back_populates="subjects"
+    )
 
 
 def create_db_and_tables() -> None:
 	Base.metadata.create_all(engine)
+      
+
+def add_user(user_id, username, name, clas, warn=0, is_admin=0, is_teacher=0, busy=0):
+    with Session() as session:  # Автоматический remove() при выходе
+        new_user = User(
+            id=user_id,
+            username=username,
+            name=name,
+            clas=clas,
+            warn=warn,
+            is_admin=is_admin,
+            is_teacher=is_teacher,
+            busy=busy
+            )
+        session.add(new_user)
+        session.commit()
+        print(f"Пользователь {name} добавлен")
+
+def add_subject(subj):
+    with Session() as session:  # Автоматический remove() при выходе
+        new_subject = Subject(subject=subj)
+        session.add(new_subject)
+        session.commit()
+        print(f"Предмет {subj} добавлен")
+
+create_db_and_tables()
+try:
+    #add_subject('geometry')
+    #add_user(54321, None, 'Raz', 10)
+    print('arr')
+except sqlalchemy.exc.IntegrityError:
+    print('unic fail')
