f005f75ce2bb1865e4b8bdb41b94da7a5c39432c
RazeManer <mr.yger001@gmail.com>
2025-09-06 15:49:48 +0300

    merged main and working branches

---
+++ TOKEN.py
@@ -0,0 +1,4 @@
+student_token = '7467162423:AAFrTJN7xvnlzbCgLxExuF0IiJ41SmoFz7o'
+teacher_token = '7811821101:AAGpsM2CxNNYeJxXezFvmbFoaCeOZOPXcT4'
+admin_token = ''
+test_bot = '6888258529:AAHokn1Roc9dDZx7vZmVKdSKsxTLq1d8MYY'
